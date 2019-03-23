f7910eacb8bf99f1b245d912485040ccf3e3203f
ivan <IvanIvanoff@users.noreply.github.com>
2019-03-23 16:18:11 +0200

    Update word_count_test.exs

--- test/word_count_test.exs
+++ test/word_count_test.exs
@@ -2,7 +2,11 @@ defmodule WordCountTest do
   use ExUnit.Case
   doctest WordCount
 
-  test "greets the world" do
-    assert WordCount.hello() == :world
+  test "true" do
+    assert 1 == 1
+  end
+  
+  test "false" do
+    assert 1 == 2
   end
 end
